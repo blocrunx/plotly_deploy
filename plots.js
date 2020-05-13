fafacd73eb981a2f95f7e431ccc7335b771bb904
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-05-13 12:33:18 -0400

    metadata filter by id complete

--- plots.js
+++ plots.js
@@ -1,37 +1,40 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
-
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
-
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
-
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
-
-// data
-var data = [trace1];
-
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    //buildCharts(newSample);
   }
-};
+  
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      //const entries = Object.entries(result);
+      Object.entries(result).forEach(([key, value]) =>
+      {PANEL.append("h6").text(key + " : " + value);});
+        
+    
+      
+      
+    });
+  }
+  
+  init();
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+
